f4f69f6fdb3570cddfef0588d7e8b05ba40996d7
YunSukHyun <czcz9207@gmail.com>
2022-04-30 16:25:06 +0900

    first is good

---
+++ syntax/template.js
@@ -0,0 +1,9 @@
+var name = 'k8808';
+var letter = 'Dear ' + name + '\n jlafjlajflajfldsfdasjdfljsda;fljsdaf';
+
+var lete = `Dear ${name}
+
+
+jlafjlajflajfldsfdasjdfljsda;fljsdaf`;
+
+console.log(lete);
